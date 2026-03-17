ff98639ef69b064ba37e7d36ba4e750dfb59aa84
Karim-Nawaz <anisul12010@yahoo.com>
2026-03-17 16:17:16 +0600

    eight commit setting change in static part

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -132,13 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
 
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
