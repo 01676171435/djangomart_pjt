931fb2d3a6bfda73b92c99ddeab348107c4154bc
Karim-Nawaz <anisul12010@yahoo.com>
2026-03-20 01:29:20 +0600

    elaven commit for cloudinary changing in setting

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import cloudinary_storage
 
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
